d08c04634dd6e59bfab959085f702d217af8a74d
Ugo DELORME <udelorme@e3r11p1.42.fr>
2016-01-08 14:28:06 +0100

    reglage bug memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: udelorme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 16:18:53 by udelorme          #+#    #+#             */
-/*   Updated: 2016/01/07 16:04:42 by udelorme         ###   ########.fr       */
+/*   Updated: 2016/01/08 14:27:35 by udelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	cpy_src = src;
 	ch = c;
 	i = -1;
-	while (++i < n && cpy_src[i - 1] != ch)
+	while (++i < n)
+	{
 		cpy_dst[i] = cpy_src[i];
-	if (cpy_src[i - 1] == ch)
-		return (&dst[i]);
+		if (cpy_src[i] == ch)
+			return (&dst[i + 1]);
+	}
 	return (NULL);
 }
