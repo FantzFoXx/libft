a1a1ab77c8fce731f12ae4a0b815052ca734cb5b
Ugo DELORME <udelorme@e3r8p3.42.fr>
2016-04-27 14:32:38 +0200

    void

--- ft_cut_cur_word.c
+++ ft_cut_cur_word.c
@@ -6,7 +6,7 @@
 /*   By: udelorme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 14:32:35 by udelorme          #+#    #+#             */
-/*   Updated: 2016/04/27 13:00:16 by udelorme         ###   ########.fr       */
+/*   Updated: 2016/04/27 14:32:20 by udelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_cut_cur_word(char *str, int index, int *start, size_t *len)
 {
-	if (str[index - 1] == ' ')
-		index--;
+	//if (str[index - 1] == ' ')
+	//	index--;
 	while (index - 1 >= 0 && str[index - 1] != ' ')
 		index--;
 	*start = index;
