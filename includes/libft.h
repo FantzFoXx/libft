28cf355b6020a60b7bbfc7bbce26a906d2fd4580
Ugo DELORME <udelorme@e3r8p3.42.fr>
2016-04-27 13:00:55 +0200

    add pointers in ft_cut

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: udelorme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 12:02:59 by udelorme          #+#    #+#             */
-/*   Updated: 2016/04/26 14:56:16 by udelorme         ###   ########.fr       */
+/*   Updated: 2016/04/27 12:59:34 by udelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,7 @@ char    		*ft_implode_tab(char **data, int start, size_t len);
 int				ft_index_fmatch(char *str, char chr);
 int				ft_index_lmatch(char *str, char chr);
 int				ft_index_nmatch(char *str, char chr, int n);
-char			*ft_cut_cur_word(char *str, int index);
+char			*ft_cut_cur_word(char *str, int index, int *start, size_t *len);
 
 /*
 ** External debug functions
